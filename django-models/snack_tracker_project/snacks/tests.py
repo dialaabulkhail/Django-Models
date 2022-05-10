fc38b5eb2383a92655cd4926e44d04e870ccdcae
dialaabulkhail <diala.sh.98@gmail.com>
2022-05-10 20:56:47 +0300

    lab27

---
+++ django-models/snack_tracker_project/snacks/tests.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+from django.urls import reverse
+
+class SnackTest(TestCase):
+    def test_snack_list_page_status_code(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_list_snack_page_template(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_list.html')
+        self.assertTemplateUsed(response, 'base.html')
